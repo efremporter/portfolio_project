40174063ebed03d41a2512e05a881002de7cb25e
efremporter <89431346+efremporter@users.noreply.github.com>
2024-05-02 13:04:01 -0700

    Retrieve image from s3 (#2)
    * Successfully connected to S3, upload working
    
    * Successfully creates a post in db after uploading image to AWS
    
    * Change route file to js, compiling error
    
    * Fix build error
    
    * Push to test deploy
    
    * Remove link
    
    * Working
    
    * Working on deploy
    
    * Fix console warning
    
    ---------
    
    Co-authored-by: efrem-kismet <efrem@kismethealth.com>

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,19 @@ import Image from "next/image";
 
 
 async function getPosts() {
-  const posts = await prisma.post.findMany()
+  const posts = await prisma.post.findFirst()
   return posts
 }
 
 export default async function Home() {
-  // const posts = await getPosts()
-  // console.log('POSTS', {posts})
+  const post = await getPosts()
+  const src = post?.filePath ?? ''
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div>
         HERE IS MY APP
       </div>
+      <Image priority={true} alt={'img'} src={src} width={200} height={200} />
     </main>
   );
 }
