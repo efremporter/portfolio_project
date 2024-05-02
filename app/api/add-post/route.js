889d135cee9209ea78bbfadf1cc7180382e68715
efrem-kismet <efrem@kismethealth.com>
2024-05-02 11:05:40 -0700

    Change route file to js, compiling error

--- app/api/add-post/route.js
+++ app/api/add-post/route.js
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prisma";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function POST(request: NextRequest) {
+export async function POST(request) {
   const req = await request.json()
   const { title, type, takenOn, filePath } = req
   const result = await prisma.post.create({
