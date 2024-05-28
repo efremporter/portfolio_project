9665800ae3e5e337b9a5602735ec5023fd87e644
efremporter <eopiscool@gmail.com>
2024-05-28 09:35:39 -0700

    Rearrange photos + vids

--- app/HomePage.tsx
+++ app/HomePage.tsx
@@ -6,15 +6,6 @@ import PostCarousel from './components/PostCarousel'
 export default function HomePage() {
   return (
     <div className='flex flex-col h-full gap-3 pt-2'>
-      <div className='flex flex-col gap-3'>
-        <div className="flex justify-between items-center">
-          <div className='text-3xl'>
-              Photos
-          </div>
-          <Link href='/photos' className='hover:underline'>View all</Link>
-        </div>
-        <PostCarousel type="photo" location='homePage' />
-      </div>
 
       <div className='flex flex-col gap-3'>
         <div className="flex justify-between items-center">
@@ -24,6 +15,16 @@ export default function HomePage() {
           <Link href='/videos' className='hover:underline'>View all</Link>
         </div>
         <PostCarousel type="video" location={'homePage'} />
+      </div>
+
+      <div className='flex flex-col gap-3'>
+        <div className="flex justify-between items-center">
+          <div className='text-3xl'>
+              Photos
+          </div>
+          <Link href='/photos' className='hover:underline'>View all</Link>
+        </div>
+        <PostCarousel type="photo" location='homePage' />
       </div>
 
       <div className='flex flex-col gap-3'>
